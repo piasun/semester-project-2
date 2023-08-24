876a62bbbc744a5f3143f916cd1a0ec1856a7810
piasun <77275966+piasun@users.noreply.github.com>
2023-08-24 09:59:25 +0200

    creatd js pages

--- js/listings.js
+++ js/listings.js
@@ -1,5 +1,6 @@
 import { auctionList } from "./functions/auctionlist.js";
 import { completeListings } from "./api/url.js";
+import { displayErrorMessage } from "./functions/errormessage.js";
 
 let url = completeListings;
 
