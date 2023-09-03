9b9976d928d341db82f1cef756d17387802f5104
piasun <77275966+piasun@users.noreply.github.com>
2023-09-03 13:44:53 +0200

    update css and auctionlist.js

--- js/functions/auctionlist.js
+++ js/functions/auctionlist.js
@@ -6,16 +6,21 @@ export function auctionList(bid) {
 //create list of auction listings
  for (let i = 0; i < bid.length; i++) {
         
-                listingContainer.innerHTML += `<article class="card">
-                                                <a href="specific-item.html?id=${bid[i].title}" class="listing">
-                                                    <img src="${bid[i].media}" />
+                listingContainer.innerHTML += `
+                                        <div class="col-lg-4 col-md-6 col-sm-12">
+                                            <a href="specific-item.html?id=${bid[i].id}" class="listing">
+                                            <div class="card mt-5">
+                                                <img src="${bid[i].media}" class="card-img" alt=".." />
+                                                <div class="listing_info">
                                                     <h3>${bid[i].title}</h3>
-                                                </a>
-                                                <p class="bid_amount">Bids: ${bid[i]._count.bids}</p>
-                                                <div class="category">
-                                                    <div>#${bid[i].tags}</div>
+                                                    <p class="bid_amount">Bids: ${bid[i]._count.bids}</p>
+                                                    <div class="category">
+                                                        <div>#${bid[i].tags}</div>
+                                                    </div>
                                                 </div>
-                                            </article>`
+                                            </div>
+                                            </a>
+                                        </div>`
             
 
      }
