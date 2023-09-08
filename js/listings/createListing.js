661cb2db1d5bae4afc91eeff1f700ae6b488b1b4
piasun <77275966+piasun@users.noreply.github.com>
2023-09-08 17:09:08 +0200

    fixing errors

--- js/listings/createListing.js
+++ js/listings/createListing.js
@@ -1,75 +1,94 @@
 import { completeListings } from "../api/url.js";
-import { displayErrorMessage } from "../functions/errormessage.js";
-import { isLoggedIn } from "../templates/nav.js"
-
-const accessToken = document.getElementById('accessToken');
-
-if(!accessToken) {
-    location.href = "/";
-}
+import { isLoggedIn } from "../templates/nav.js";
 
 isLoggedIn();
 
-const form = document.querySelector("form");
-const itemTitle = document.querySelector("#itemTitle");
-const itemContent = document.querySelector("#itemContent");
-const itemImage = document.querySelector("#summary");
-const bidEnds = document.querySelector(".bidEnds");
-const message = document.querySelector(".message-container");
 
-form.addEventListener("submit", submitForm);
+const itemTitle = document.querySelector(".itemTitle");
+const itemContent = document.querySelector(".itemContent");
+const itemImage = document.querySelector(".itemImage");
+const endsBid = document.querySelector(".endBid");
+const submitItemBtn = document.querySelector(".submitItem");
 
-function submitForm(event) {
+const createListing = completeListings;
+
+export async function addListing(url, data) {
+    try {
+        const accessToken = localStorage.getItem('accessToken'); 
+        const options = {
+            method: 'POST', 
+            headers : {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(data),
+        };
+       console.log("Url:", url,"Data:", data,"Options:", options);
+
+        const response = await fetch(url, options); 
+        const answer = await response.json();
+
+        if (answer.id) {
+            window.location = "./index.html";
+          }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+const titleMsg = document.getElementById("titleMsg");
+const descMsg = document.getElementById("descMsg");
+const mediaMsg = document.getElementById("mediaMsg");
+const endbidMsg = document.getElementById("endbidMsg");
+
+submitItemBtn.addEventListener('click', createNewListingForm);
+function createNewListingForm(event) {
     event.preventDefault();
-
-    message.innerHTML = "";
 
     const title = itemTitle.value.trim();
     const description = itemContent.value.trim();
-    const media = itemImage.value.trim();
-    const endsAt = bidEnds.value.trim();
+    const itemImg = itemImage.value.trim();
+    const endsAt = endsBid.value.trim();
 
-    if(title.length === 0 || description.length === 0 || media.length === 0 || endsAt.length === 0) {
-        return displayMessage("warning", "Please supply proper values", "message-container");
+    const listingData = {
+        title: title,
+        description: description,
+        media: media,
+        endsAt: endsAt,   
+       };
+    console.log(listingData);
+
+    const enteredTitle = title;
+    titleMsg.innerHTML = "";
+     if (enteredTitle.length < 1) {
+     titleMsg.innerHTML = 'Your title has to be at least 1 or more characters.';
+     }
+     
+    descMsg.innerHTML = "";
+    const enteredDesc = description;
+    if (enteredDesc.length < 1) {
+        descMsg.innerHTML = 'Your description has to be at least 1 or more characters.';
     }
 
-    createBid(title, description, media, endsAt);
-}
+    const enteredMedia = itemImg;
+    mediaMsg.innerHTML = "";
+    let itemMedia = /\.(jpeg|jpg|gif|png|svg)$/;
+    if (enteredMedia === "") {
+        enteredMedia.innerHTML = "There is no image of the item";
+    } else if (!enteredMedia.value == "" && !itemMedia.test(enteredMedia))
+    { enteredMedia.innerHTML = "Please enter a valid image url!";}
 
-async function createBid(title, description, media) {
-    const url = completeListings;
-
-    const data = JSON.stringify({ title: title, description: description, media: media, endsAt: endsAt});
-
-    const accessToken = document.getElementById('accessToken');
-
-    const options = {
-        method: "POST", 
-        body: data, 
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`
-        },
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const json = await response.json();
-
-        if(json.created_at) {
-            displayMessage("success", "Article added succesfully", ".message-container")
-            form.reset();
-        }
-
-        if(json.error) {
-            displayMessage("error", "json.message", ".message-container")
-        }
-
-    }
-    catch (error) {
-        displayMessage("error", "An error occured", ".message-container")
-
+    endbidMsg.innerHTML = "";
+    const enteredEndbid = endsAt;
+    if (enteredEndbid === "") {
+        endbidMsg.innerHTML = 'You have to add a date and time to end your auction!'
     }
 
-
+      if (titleMsg.innerHTML === "" && descMsg.innerHTML === "" && mediaMsg.innerHTML === "") {
+        console.log("Form is submitted!");
+     }
+     else {
+        console.log("Are you sure you have everything right?");
+    }
+    addListing(createListing, listingData);
 }
